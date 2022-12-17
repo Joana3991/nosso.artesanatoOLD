4ebc0e3e80a0979caa6141c2b291b801d0a5eafa
Joana Areas <jo_areas@hotmail.com>
2022-12-16 22:19:08 -0300

    added produtos validations

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,6 @@
 class Produto < ApplicationRecord
+  validates :nome, presence: true
+  validates :nome, uniquenesse: true
+  validates :tamanho, inclusion: { in: %w[S M L], message: "%{value} is not a valid size" }
+  validates :categoria, inclusion: { in: %w[bandeja caixa miniatura outros] }
 end
